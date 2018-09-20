3fba64c11c6e97abf89400d504dfe6fe82d7f72f
tylia <tylia.cordata@gmail.com>
2018-09-20 19:57:56 +0300

    toLowerCase() v2

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -6,7 +6,33 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        if (Math.abs(Character.toLowerCase(x) - Character.toLowerCase(y)) == N) {
+        if (Character.isUpperCase(x) || Character.isUpperCase(y)) {
+            char lowerX = toLowerIfUpper(x);
+            if(byN(lowerX, y)) {
+                return true;
+            }
+
+            char lowerY = toLowerIfUpper(y);
+            if(byN(lowerY, y)) {
+                return true;
+            }
+        }
+
+        if (Math.abs(x - y) == N) {
+            return true;
+        }
+        return false;
+    }
+
+    private char toLowerIfUpper(char x) {
+        if (Character.isUpperCase(x)) {
+            return Character.toLowerCase(x);
+        }
+        return x;
+    }
+
+    private boolean byN(char x, char y) {
+        if (Math.abs(x - y) == N) {
             return true;
         }
         return false;
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,33 @@
 public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
-        if (Math.abs(Character.toLowerCase(x) - Character.toLowerCase(y)) == 1) {
+        if (Character.isUpperCase(x) || Character.isUpperCase(y)) {
+            char lowerX = toLowerIfUpper(x);
+            if(byOne(lowerX, y)) {
+                return true;
+            }
+
+            char lowerY = toLowerIfUpper(y);
+            if(byOne(lowerY, y)) {
+                return true;
+            }
+        }
+
+        if (Math.abs(x - y) == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private char toLowerIfUpper(char x) {
+        if (Character.isUpperCase(x)) {
+            return Character.toLowerCase(x);
+        }
+        return x;
+    }
+
+    private boolean byOne(char x, char y) {
+        if (Math.abs(x - y) == 1) {
             return true;
         }
         return false;
